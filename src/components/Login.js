b383d63ab4d2108f7b25c6f453e7fcdd75bb412a
Elijah Samuels <elijahsamuels@gmail.com>
2021-08-24 16:13:26 -0600

    removed parent div for nav.js. Sticky appbar works.

--- src/components/Login.js
+++ src/components/Login.js
@@ -22,7 +22,7 @@ const Login = (props) => {
         <div>
             <GoogleLogin
                 clientId={clientId}
-                buttonText="Login"
+                buttonText="Google Login"
                 onSuccess={onSuccess}
                 onFailure={onFailure}
                 cookiePolicy={"single_host_origin"}
--- src/components/staticComponents/Nav.js
+++ src/components/staticComponents/Nav.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -8,12 +9,10 @@ import {
     Toolbar,
     IconButton,
     Typography,
-    SvgIcon,
+    // Slide,
+    // useScrollTrigger,
     makeStyles,
 } from "@material-ui/core";
-
-import { Image } from "semantic-ui-react";
-// import { AppBar, Image, Sticky, Icon } from "semantic-ui-react";
 
 import logo from "../../images/sampleLogo24x24.svg";
 import { editUser } from "../../actions/userActions.js";
@@ -31,92 +30,102 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-const LogoIcon = (props) => {
-    return (
-        <SvgIcon {...props}>
-            <path d={logo} />
-        </SvgIcon>
-    );
-};
+// function HideOnScroll(props) {
+//     const { children, window } = props;
+//     // Note that you normally won't need to set the window ref as useScrollTrigger
+//     // will default to window.
+//     // This is only being set here because the demo is in an iframe.
+//     const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+//     return (
+//       <Slide appear={false} direction="down" in={!trigger}>
+//         {children}
+//       </Slide>
+//     );
+//   }
+
+//   HideOnScroll.propTypes = {
+//     children: PropTypes.element.isRequired,
+//     /**
+//      * Injected by the documentation to work in an iframe.
+//      * You won't need it on your project.
+//      */
+//     window: PropTypes.func,
+//   };
 
 const Nav = (props) => {
     const classes = useStyles();
     const currentUser = props.currentUser;
     return (
-        <div>
-            <AppBar className={classes.root} position="static" elevation={0}>
-                <Toolbar>
-                    <Typography>
+        <AppBar className={classes.root} position="sticky" elevation={0}>
+            <Toolbar>
+                <Typography>
+                    <Link to="/" style={{ textDecoration: "none" }}>
+                        <Icon classes={{ root: classes.iconRoot }}>
+                            <img
+                                className={classes.imageIcon}
+                                src={logo}
+                                alt="sample SVG icon"
+                            />
+                        </Icon>
+                    </Link>
+
+                    <IconButton
+                        name="Main"
+                        disableRipple="true"
+                        size="small"
+                        className={classes.button}>
                         <Link to="/" style={{ textDecoration: "none" }}>
-                            <Icon classes={{ root: classes.iconRoot }}>
-                                <img
-                                    className={classes.imageIcon}
-                                    src={logo}
-                                    alt="sample SVG icon"
-                                />
-                            </Icon>
+                            theHang
                         </Link>
+                    </IconButton>
 
-                        <IconButton
-                            name="Main"
-                            disableRipple="true"
-                            size="small"
-                            className={classes.button}>
-                            <Link to="/" style={{ textDecoration: "none" }}>
-                                 theHang
-                            </Link>
-                        </IconButton>
+                    <IconButton
+                        name="About"
+                        disableRipple="true"
+                        size="small"
+                        className={classes.button}>
+                        <Link to="/about" style={{ textDecoration: "none" }}>
+                            About
+                        </Link>
+                    </IconButton>
 
-                        <IconButton
-                            name="About"
-                            disableRipple="true"
-                            size="small"
-                            className={classes.button}>
-                            <Link
-                                to="/about"
-                                style={{ textDecoration: "none" }}>
-                                About
-                            </Link>
-                        </IconButton>
+                    <IconButton
+                        name="Musicians"
+                        disableRipple="true"
+                        size="small"
+                        className={classes.button}>
+                        <Link
+                            to="/musicians"
+                            style={{ textDecoration: "none" }}>
+                            Musicians
+                        </Link>
+                    </IconButton>
 
-                        <IconButton
-                            name="Musicians"
-                            disableRipple="true"
-                            size="small"
-                            className={classes.button}>
-                            <Link
-                                to="/musicians"
-                                style={{ textDecoration: "none" }}>
-                                Musicians
-                            </Link>
-                        </IconButton>
-
-                        {!!currentUser ? (
-                            <>
-                                <IconButton
-                                    name="Edit"
-                                    className={classes.button}
-                                    to={`/musician/${currentUser.id}/edit`}>
-                                    <Link
-                                        to={`/musician/${currentUser.id}/edit`}>
-                                        <Icon loading name="setting" />
-                                        Edit
-                                    </Link>
-                                </IconButton>
-                                <Logout />
-                            </>
-                        ) : (
+                    {!!currentUser ? (
+                        <>
                             <IconButton
-                                name="Login"
-                                disableRipple="true"
-                                className={classes.button}>
-                                <Login />
+                                name="Edit"
+                                className={classes.button}
+                                to={`/musician/${currentUser.id}/edit`}>
+                                <Link to={`/musician/${currentUser.id}/edit`}>
+                                    <Icon loading name="setting" />
+                                    Edit
+                                </Link>
                             </IconButton>
-                        )}
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
+                            <Logout />
+                        </>
+                    ) : (
+                        <IconButton
+                            name="Login"
+                            disableRipple="true"
+                            className={classes.button}>
+                            <Login />
+                        </IconButton>
+                    )}
+                </Typography>
+            </Toolbar>
+        </AppBar>
     );
 };
 
